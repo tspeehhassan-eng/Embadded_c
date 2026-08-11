5adade04d3af54f9b2a163c34fe0f5717dc8a25f
tspeehhassan-eng <tspeehhassanformal@gmail.com>
2026-08-12 00:02:17 +0300

    array task

---
+++ coped_array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include"../std_types.h"
+int main(void ){
+	S8 arr[100],var;
+	S8 freq[100]={0};
+	U16 counter=0;
+	for (int i=0;i<10;i++){
+		scanf(" %c",&var);
+		if(freq[var]==0){
+			arr[counter]=var;
+			freq[var]=1;
+			counter++;
+			}
+	}
+for (int i=0;i<counter;i++){
+	printf(" %c ",arr[i]);
+}}
