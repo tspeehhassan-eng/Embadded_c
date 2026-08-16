8585bdae369bf3e39bcc5eb6a9b5c37d3ae60e83
tspeehhassan-eng <tspeehhassanformal@gmail.com>
2026-08-16 16:01:11 +0300

    Move discussion files to discussion_1

--- discussion_1/MinMaxIndex.c
+++ discussion_1/MinMaxIndex.c
@@ -3,18 +3,21 @@
 #include"../std_types.h"
 
 U32 max_ind,min_ind;
-
+S32 max , min;
 void getMaxMin(S32 arr[10]);
 
 int main(void){
 	S32 arr[10]={1,2,3,5,10,9,8,7,6,0};
 	getMaxMin(arr);
-	printf("the max number at index %lu\n",max_ind);
-	printf("the min number at index %lu\n",min_ind);
-}
+	
+	printf("the max number is at index %d ",max);
+	printf(" and its index %lu \n",max_ind);
+
+    printf("the min number is at index %d ",min);
+	printf(" and its index %lu \n",min_ind);}
 void getMaxMin(S32 arr[10]){
-	S32 max=arr[0];
-	S32 min=arr[0];
+	max=arr[0];
+	min=arr[0];
 	for(S32 i=0;i<10;i++){
 		if(arr[i]>max){
 			max=arr[i];
